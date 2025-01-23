ec38e6704a9e3cf35843fb2d5b128c095fb16c51
parth <parthrk75@gmail.com>
2025-01-23 22:19:26 +0530

    first

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.example.config;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .packagesToScan("com.example.controller") // Specify the package of your controllers
-                .pathsToMatch("/**") // Include all paths
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0.0")
+                        .description("This is a simple Swagger API documentation for your Spring Boot project."));
     }
 }
